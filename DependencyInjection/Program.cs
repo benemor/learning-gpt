8bfc9d067af0fba1bfbd774fe6e1df1a0675c9c7
benemor <benemor.tv@gmail.com>
2023-04-01 01:14:21 -0400

    DependencyInjection demo

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using DependencyInjection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterType<GreetingService>().As<IGreetingService>();
+        builder.RegisterType<MyClass>();
+        var container = builder.Build();
+        var myClass = container.Resolve<MyClass>();
+        myClass.DoSomething();
+    }
+}
+
+public class MyClass
+{
+    private readonly IGreetingService _greetingService;
+
+    public MyClass(IGreetingService greetingService)
+    {
+        _greetingService = greetingService;
+    }
+
+    public void DoSomething()
+    {
+        _greetingService.Greet("John");
+    }
+}
